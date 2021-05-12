3cf9a97801ef7bb93a4a41b7709955789b0d34eb
Felipe Mandu Pereira <fmandu@pagseguro.com>
2021-05-12 17:11:47 -0300

    retirada de verificação de erros do controller e mudança da construção do object ResponseEntity

--- src/main/kotlin/felipemandu/com/br/mangaappstudy/controller/AuthorController.kt
+++ src/main/kotlin/felipemandu/com/br/mangaappstudy/controller/AuthorController.kt
@@ -2,15 +2,13 @@ package felipemandu.com.br.mangaappstudy.controller
 
 import felipemandu.com.br.mangaappstudy.dto.AuthorInputDTO
 import felipemandu.com.br.mangaappstudy.dto.AuthorOutputDTO
-import felipemandu.com.br.mangaappstudy.exception.AuthorNotFound
-import felipemandu.com.br.mangaappstudy.exception.EntityNotFoundException
 import felipemandu.com.br.mangaappstudy.service.interfaces.AuthorService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -19,29 +17,22 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import java.net.URL
+import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
 
-@Controller
+@RestController
 @RequestMapping("/authors")
 class AuthorController(@Autowired val authorService: AuthorService) {
 
-    val host = "http://localhost:8080/authors"
 
     @PostMapping("/")
     fun createAuthor(@RequestBody @Valid authorInputDTO: AuthorInputDTO): ResponseEntity<ResponseWrapper<AuthorOutputDTO>> {
-        val response = try {
-            val author = authorService.create(authorInputDTO)
-            val resourceAddress = URL("$host/id/${author.id}")
-
-            ResponseWrapper(status = HttpStatus.CREATED.value(), resource = author, resourceAddress = resourceAddress)
-        } catch (e: AuthorNotFound) {
-            val error: Error = Error(AuthorNotFound::class.simpleName!!, e.localizedMessage!!)
-            ResponseWrapper(status = HttpStatus.NOT_FOUND.value(), error = error)
-        }
-
-        return ResponseEntity(response, HttpStatus.resolve(response.status)!!)
+        val author = authorService.create(authorInputDTO)
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .header(HttpHeaders.LOCATION, "authors/id/${author.id}")
+            .body(ResponseWrapper(resource = author))
     }
 
     @GetMapping("/")
@@ -50,30 +41,25 @@ class AuthorController(@Autowired val authorService: AuthorService) {
         @RequestParam(name = "size", required = false, defaultValue = "20") size: Int,
         @RequestParam(name = "sort", required = false, defaultValue = "id") sort: String,
         @RequestParam(name = "dir", required = false, defaultValue = "ASC") dir: String
-    ): ResponseEntity<ResponseWrapper<List<AuthorOutputDTO>>> {
-        val response = try {
-            val author = authorService.findAll(PageRequest.of(page, size, Sort.Direction.valueOf(dir), sort))
-            ResponseWrapper(status = HttpStatus.OK.value(), resource = author)
-        } catch (e: AuthorNotFound) {
-            val error: Error = Error(AuthorNotFound::class.simpleName!!, e.localizedMessage!!)
-            ResponseWrapper(status = HttpStatus.NOT_FOUND.value(), error = error)
-        }
-        return ResponseEntity(response, HttpStatus.resolve(response.status)!!)
+    ): ResponseEntity<ResponseWrapper<AuthorOutputDTO>> {
+        val author = authorService.findAll(PageRequest.of(page, size, Sort.Direction.valueOf(dir), sort))
+        val resources = ResponseWrapper(
+            resources = author.content,
+            page = page,
+            size = size,
+            numberOfPages = author.totalPages
+        )
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(resources)
     }
 
     @GetMapping("/id/{id}")
     fun getAuthorById(@PathVariable("id") id: Long): ResponseEntity<ResponseWrapper<AuthorOutputDTO>> {
-        val response = try {
-            val author = authorService.findById(id)
-
-            ResponseWrapper(status = HttpStatus.OK.value(), resource = author)
-        } catch (e: EntityNotFoundException) {
-
-            val error: Error = Error(AuthorNotFound::class.simpleName!!, e.localizedMessage!!)
-            ResponseWrapper(status = HttpStatus.NOT_FOUND.value(), error = error)
-        }
-
-        return ResponseEntity(response, HttpStatus.resolve(response.status)!!)
+        val author = authorService.findById(id)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ResponseWrapper(resource = author))
     }
 
     @DeleteMapping("/id/{id}")
@@ -87,22 +73,10 @@ class AuthorController(@Autowired val authorService: AuthorService) {
         @PathVariable id: Long,
         @RequestBody authorDto: AuthorInputDTO
     ): ResponseEntity<ResponseWrapper<AuthorOutputDTO>> {
-        val response: ResponseWrapper<AuthorOutputDTO> = try {
-            ResponseWrapper(status = 200, resource = authorService.update(id, authorDto))
-        } catch (e: Exception) {
-            if (e is EntityNotFoundException)
-                ResponseWrapper(
-                    status = HttpStatus.NOT_FOUND.value(),
-                    error = Error(AuthorNotFound::class.simpleName!!, e.localizedMessage!!)
-                )
-            else
-                ResponseWrapper(
-                    status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                    error = Error(e::class.simpleName!!, e.localizedMessage!!)
-                )
-        }
-
-        return ResponseEntity(response, HttpStatus.resolve(response.status)!!)
+        val resource = authorService.update(id, authorDto)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(ResponseWrapper(resource = resource))
     }
 
 
